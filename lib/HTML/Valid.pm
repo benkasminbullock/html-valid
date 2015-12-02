d4b6649b5522fc69787b427f7f1734836b145661
Ben Bullock <benkasminbullock@gmail.com>
2015-12-03 08:51:36 +0900

    Version in pm to 0.02_01.

--- lib/HTML/Valid.pm
+++ lib/HTML/Valid.pm
@@ -9,7 +9,7 @@ use warnings;
 use strict;
 use Carp;
 use JSON::Parse 'json_file_to_perl';
-our $VERSION = '0.02';
+our $VERSION = '0.02_01';
 require XSLoader;
 XSLoader::load ('HTML::Valid', $VERSION);
 
